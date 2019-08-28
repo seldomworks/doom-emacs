8bd87c2dc3762255785dffcf9278a0a9d2fd4154
Anish Muttreja <amuttrej@qti.qualcomm.com>
2019-08-28 13:29:41 -0600

    use jupyter

--- modules/lang/org/packages.el
+++ modules/lang/org/packages.el
@@ -2,6 +2,7 @@
 ;;; lang/org/packages.el
 
 (package! org-plus-contrib)  ; install cutting-edge version of org-mode
+(package! jupyter)
 (package! org-bullets :recipe (:host github :repo "Kaligule/org-bullets"))
 (package! toc-org)
 (when (featurep! :editor evil)
@@ -43,3 +44,4 @@
   (package! centered-window :recipe (:host github :repo "anler/centered-window-mode"))
   (package! org-tree-slide)
   (package! ox-reveal))
+
